ba12e92a0169cb4c67a34e1ce1d2116d31dd76b1
Sergey <yojjoy1989@gmail.com>
2023-08-14 18:30:38 +0400

    pagination style

--- src/components/pagination/styled.ts
+++ src/components/pagination/styled.ts
@@ -11,6 +11,15 @@ export const Container = styled.div`
 
 export const Button = styled.button`
     margin: 5px;
+    background-color: #fff;
+    border: 1px solid #efefef;
+    outline: 0;
+    padding: 6px 8px;
+    border-radius: 3px;
     cursor: pointer;
+    &.active {
+        border: 1px solid #c1c1c1;
+        font-weight: bold;
+    }
 `;
 
